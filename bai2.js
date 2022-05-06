f957fb648795f2f4093cb180d6ba30490e5b9464
PhgX <phuongbm211@gmail.com>
2022-05-06 11:22:47 +0700

    done

---
+++ bai2.js
@@ -0,0 +1,17 @@
+
+let inputWidth;
+let inputHeight;
+
+inputWidth = prompt("Enter the width");
+inputHeight = prompt("Enter the height");
+
+let width= parseFloat(inputWidth);
+let height= parseFloat(inputHeight);
+let area = width*height;
+
+
+document.write("Enter width = " + width + "<br>");
+document.write("Enter height = " + height + "<br>"
+);
+document.write("The area is " + area + "<br>"
+);
