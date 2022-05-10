b31436e8948c980517080cf98db96229a2f4de87
PhgX <phuongbm211@gmail.com>
2022-05-10 23:22:23 +0700

    done

---
+++ baitap.js
@@ -0,0 +1,12 @@
+    function iloveyou()
+    {
+        alert("Yes. you are right ");
+    }
+    function dichuyen()
+    {
+        let x = parseInt(Math.random() * 500);
+        let y = parseInt(Math.random() * 500);
+        document.getElementById('NO').style.left = x + 'px';
+        document.getElementById('NO').style.top = y + 'px';
+    }
+   
