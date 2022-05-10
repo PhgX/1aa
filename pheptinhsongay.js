3f447b4a01cab4772e9a045f6c6e5b980967c0e8
PhgX <phuongbm211@gmail.com>
2022-05-10 08:02:35 +0700

    sua lai ngay

--- pheptinhsongay.js
+++ pheptinhsongay.js
@@ -3,14 +3,19 @@ function myFunc() {
     let b = document.getElementById("demo");
     if (a > 0 && a < 13) {
         switch (a) {
+            case 3:
+            case 5:
+            case 8:
+            case 9:
+            case 11:
             case 1:
-                b.innerHTML = "Tháng 1 có 31 ngày";
+                b.innerHTML = "Tháng" + " " + a + " " +  "có 31 ngày";
                 break;
             case 2:
-                b.innerHTML = "Tháng 2 có 28 hoặc 29 ngày";
+                b.innerHTML = "Tháng" + " " + a + " " +  "có 28 hoặc 29 ngày";
                 break;
             default:
-                b.innerHTML = "Tháng " + a + " " + "có 30 ngày";
+                b.innerHTML = "Tháng" + " " + a + " " + "có 30 ngày";
                 break;
         }
     }
