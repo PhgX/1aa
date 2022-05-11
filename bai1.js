4814972b9622ddb74fd17992065911f0032b2488
PhgX <phuongbm211@gmail.com>
2022-05-11 13:24:43 +0700

    done

---
+++ bai1.js
@@ -0,0 +1,7 @@
+// Sử dụng vòng lặp do while để viết lại chương trình yêu cầu nhập các số từ 1 -> 10
+// ở ví dụ trong phần vòng lặp while.
+let num = '';
+do {
+    num = prompt("Nhập số từ 1 đến 10");
+}
+while (num > 0 && num <11);
